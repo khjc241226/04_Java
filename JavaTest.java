a2291b504a6b105a5e523fc789e98ffb85dd7ff6
khjc241226 <khjc241226@gmail.com>
2025-01-20 11:25:31 +0900

    java workspace 등록

---
+++ JavaTest.java
@@ -0,0 +1,16 @@
+
+public class JavaTest{
+
+  public static void main(String[] args){
+    
+     System.out.println("Hello World!!!");
+     System.out.println("안녕하세요");
+     System.out.println("아 언제 금요일...");
+
+     for(int i=1 ; i<=5 ; i++){
+       System.out.println(i + "번째 줄 출력");
+     }
+
+
+  }
+}

98c968e403246ccec99f3cb45babe27ee077e987
khjc241226 <khjc241226@gmail.com>
2025-01-24 17:07:39 +0900

    20240124_static, 객체 배열

--- 05_class_object/src/staticmember/StaticMemberRun.java
+++ 05_class_object/src/staticmember/StaticMemberRun.java
@@ -47,6 +47,15 @@ public class StaticMemberRun {
 		System.out.println(k1.toString());
 		System.out.println(k2.toString());
 		
+		
+		System.out.println("-------------------------------------");
+		System.out.println("[초기화 블럭 확인]");
+		
+		Korean k3 = new Korean();
+		Korean k4 = new Korean();
+		System.out.println(k3.toString());
+		System.out.println(k4.toString());
+		
 	}
 }
 
